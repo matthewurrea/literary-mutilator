c82563d63b2e4036672d0b34ae742e1e4a13326c
Matthew Urrea <matt.urrea.code@gmail.com>
2020-02-24 00:29:23 -0700

    completed change font

---
+++ change-font/script.js
@@ -0,0 +1,5 @@
+//function that changes the color of all three paragraphs to Comic Sans 
+function changeFont() {
+	document.getElementById("text").style.fontFamily = "Comic Sans MS";
+}
+
---
+++ map-scoll-event/script.js
@@ -0,0 +1,1 @@
+//function that deletes one word at a time on scroll event
