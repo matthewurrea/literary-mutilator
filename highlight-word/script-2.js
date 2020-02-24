73ad5e09ac4bc0895ad8873ffd53e5798f2840da
Matthew Urrea <matt.urrea.code@gmail.com>
2020-02-24 01:47:55 -0700

    completed highlight-word

---
+++ highlight-word/script-2.js
@@ -0,0 +1,10 @@
+//function that highlights a word 
+function highlight(text) {
+  var inputText = document.getElementById("inputText");
+  var innerHTML = inputText.innerHTML;
+  var index = innerHTML.indexOf(text);
+  if (index >= 0) { 
+   innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+   inputText.innerHTML = innerHTML;
+  }
+}
